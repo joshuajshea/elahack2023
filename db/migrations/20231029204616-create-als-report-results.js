2a13889777269d47d73a64628ccb2f41ba69c3c3
admin <admin@admins-MacBook-Pro.local>
2023-10-29 22:47:01 -0500

    DB initial setup, sequelize error w config will have to be worked around

---
+++ db/migrations/20231029204616-create-als-report-results.js
@@ -0,0 +1,55 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('als_report_results', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      analyte: {
+        type: Sequelize.STRING
+      },
+      als_sample_id: {
+        type: Sequelize.STRING
+      },
+      client_sample_id: {
+        type: Sequelize.STRING
+      },	
+      method: {
+        type: Sequelize.STRING 
+      },	
+      results: {
+        type: Sequelize.DOUBLE 
+      },	
+      detection_limit: {
+        type: Sequelize.DOUBLE 
+      }, 
+      units: {
+        type: Sequelize.STRING 
+      },	
+      date_sampled: {
+        type: Sequelize.DATE 
+      },	
+      analysis_date: {
+        type: Sequelize.DATE 
+      },
+      processed: {
+        type: Sequelize.BOOLEAN
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('als_report_results');
+  }
+};
