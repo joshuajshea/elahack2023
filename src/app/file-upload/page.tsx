8daa3a8c9b9511f4229d12a9d8d23d5c90a678a3
admin <admin@admins-MacBook-Pro.local>
2023-11-01 22:02:52 -0500

    building xlsx uploader, need to style, need to have the data go somewhere (probaby built into sequelize obj and returned to db, but its possible to not have these stored in DB and output straight to PDF)

---
+++ src/app/file-upload/page.tsx
@@ -0,0 +1,81 @@
+'use client';
+import * as XLSX from 'xlsx';
+
+export default function FileUpload() {
+
+    interface FileRow {
+        als_sample_id?: string;
+        analysis_date?: string;
+        analyte?: string;
+        client_sample_id?: string;
+        date_sampled?: string;
+        detection_limit?: string;
+        hold_time_eval?: string;
+        matrix?: string;
+        method?: string;
+        prep_date?: string;
+        qc_eval?: string;
+        qc_lot?: string;
+        qual?: string;
+        results?: string;
+        "sub-matrix"?: string;
+        time_sampled?: string;
+        units?: string;
+    }
+
+    const parseWorkbook = (workbook: XLSX.WorkBook) => {
+        const report = workbook.Sheets?.["Detailed Report"]
+        if(!report) return;
+        const range = report['!autofilter']?.ref.split(':');
+        if(!range) return;
+
+        let results: Array<FileRow> = [];
+        let headers: Array<string> = [];
+        const sheetAlphaRange = [range[0].charCodeAt(0), range[1].charCodeAt(0)];
+        const sheetBetaRange = [parseInt(range[0].slice(1)), parseInt(range[1].slice(1))];
+
+        for(let i = sheetAlphaRange[0]; i <= sheetAlphaRange[1]; i++) {
+                let cell: string = `${String.fromCharCode(i)}9`
+                console.log(cell);
+                headers.push(
+                    (report[cell]?.v)
+                        .toString()
+                        .toLowerCase()
+                        .trim()
+                        .replaceAll(' ', '_'));
+        }
+        for(let i = sheetBetaRange[0] + 10; i <= sheetBetaRange[1]; i++) {
+            let entry: FileRow = {};
+            let k = 0;
+            for(let j = sheetAlphaRange[0]; j <= sheetAlphaRange[1]; j++) {
+                let cell: string = `${String.fromCharCode(j)}${i}`
+                entry[`${headers[k]}` as keyof FileRow] = report[`${cell}`]?.v; 
+                k++;
+            }
+            results.push(entry);
+        }
+
+        results = results.filter(result => !!result.als_sample_id);
+
+        //TODO: BUILD API TO UPLODD THIS TO DB
+        return results;
+    }
+
+    const handleDropAsync = (e: any) => {
+        var file = e.target.files[0];
+        var reader = new FileReader();
+        if(!e.target) return;
+        reader.onload = function(e) {
+            var workbook = XLSX.read(e.target?.result);
+            parseWorkbook(workbook);
+        }
+        reader.readAsArrayBuffer(file);
+    }
+
+    return (
+    <div className="file-upload">
+        <form className="file-upload__upload" onSubmit={(e) => e.preventDefault()}>
+            <input type="file" id="input-file-upload" multiple={true} onChange={(e) => handleDropAsync(e)}/>
+        </form>
+    </div>);
+}
